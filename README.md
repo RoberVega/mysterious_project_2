e6f1619339e870159d7db4f9959c41279b1d333f
Roberto Vega <rober.vega93@gmail.com>
2023-08-22 07:13:26 +0100

    Initial files

---
+++ README.md
@@ -0,0 +1,4 @@
+# Python Mystery Structure Nº 3
+
+This repo serves as a reference point for a mysterious structure to practise concepts.
+
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name = 'mysterious_project_3',
+    version = '0.1.0',
+    packages = ['mysterious_package_3']
+    )
